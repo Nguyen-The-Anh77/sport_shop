5f08575486bb1a827dd07a8895130c7a22ff20ff
DucLongDepTrai <nguyenduclong98765@gmail.com>
2026-07-09 09:26:40 +0700

    sua loi tieng viet

--- views/include/header.php
+++ views/include/header.php
@@ -81,7 +81,7 @@
 							</div>
 							<div class="modern-action-label">
 								<strong>Đăng nhập</strong>
-								<span>Truy cập tài khoản</span>
+								<span>Truy cập </span>
 							</div>
 						</a>
 						<?php } ?>
